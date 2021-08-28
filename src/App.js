a3fa4fee604c32441198ad1681bb1f015f5f0ef4
Maurya <surajaaa26746@gmail.com>
2021-08-28 11:57:14 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ const  App = ()=> {
     object =(await respose.json());
     console.log(object);
     setUsers(object["data"]);
-   
     console.log("hello Neeraj welcome");
   }
 useEffect( ()=>{
@@ -17,40 +16,33 @@ useEffect( ()=>{
 },[])
   return (
     <>
-    <div className="container-fluid ">
-    <div className="row">
-   <div className="col-2 border">
-     </div>
-         <div className="col-10  border"> 
-         <div className = "container-fluid">
          <div className="row">
             {
           users.map((curElem) =>{
           const {first_name,last_name,avatar,email} = curElem;
           return(
-            <div className = "col-12 col-lg-3 col-md-4 col-sm-12">
+            <div className = "col-lg-3 col-md-4 col-sm-12">
             <div className="card mt-3 cards1">
                      <img src={avatar} alt="neeraj img" className = "rounded img-responsive card-img-top" width="250" height="150"/>
                            <div className= "card-body"> 
-                           <h4 className = "acard-text"> {first_name}</h4>
-                           <p className = "acard-text"> {last_name}</p> 
-                           <p className = "acard-text"> {email}</p> 
+                           <h6 className = "inline">first_name :-</h6>
+                           <p className = "inline acard-text"> {first_name}</p>
+                           <br></br>
+                           <h6 className = "inline">last_name :-</h6>
+                           <p className = " inline acard-text"> {last_name}</p>
+                           <br/>
+                           <h6 className = "inline">Email :-</h6> 
+                           <p className = " inline acard-text"> {email}</p> 
                            </div>
                            </div>
                      </div>
-                
-              
-             
-                  
                       )
                 })  
             }
             </div>
      
-                  </div>
-           </div>
-           </div>
-           </div>
+                
+
 
         </>
     
